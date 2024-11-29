8688c9ea92187a55ba7d6da78e71e6dad49b206d
zhuangguangtuan <zhuangguangtuan@gmail.com>
2024-11-29 16:00:48 +0800

    feat: reminderProvider schema 🚧

---
+++ app/src/main/java/tech/igrant/remindcat/reminder/ReminderProvider.kt
@@ -0,0 +1,14 @@
+package tech.igrant.remindcat.reminder
+
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ReminderProvider {
+
+    @GET("reminders")
+    suspend fun list(): List<Reminder>
+
+    @POST("reminders")
+    suspend fun save(reminder: Reminder): Reminder
+
+}
