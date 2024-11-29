3761df57db93e038af5cf7d6a0e5762c4db563d1
zhuangguangtuan <zhuangguangtuan@gmail.com>
2024-11-29 10:50:27 +0800

    chore: upgrade dependencies and target sdk ⬆️

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "tech.igrant.remindcat"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "tech.igrant.remindcat"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
